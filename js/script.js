d5d9c0059b5ff52a93c7dde41b7ee739176f2366
omartamer79 <omartameralkeblawy@gmail.com>
2026-06-09 14:48:13 +0300

    all page

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-const toggleBtn = document.querySelector(".toggle");
-const toggleBtnIcon = document.querySelector(".toggle i");
-const dropdownMenu = document.querySelector(".dropdowin");
+const toggleBtn = document.querySelector(".toggle")
+const toggleBtnIcon = document.querySelector(".toggle i")
+const dropdownMenu = document.querySelector(".dropdowin")
 
-toggleBtn.onclick = function () {
-  dropdownMenu.classList.toggle("open");
-  const isOpen = dropdownMenu.classList.contains("open");
+// toggleBtn.onclick = function () {
+//     dropdownMenu.classList.toggle("open")
+//     const isOpen = dropdownMenu.classList.contains("open")
 
-  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
-};
+//     toggleBtnIcon.classList = isOpen
+//     ? "fa-solid fa-xmark"
+//     : "fa-solid fa-bars"
+// }
+
+function openMenu() {
+    dropdownMenu.classList.toggle("open")
+    const isOpen = dropdownMenu.classList.contains("open")
+
+    toggleBtnIcon.classList = isOpen
+    ? "fa-solid fa-xmark"
+    : "fa-solid fa-bars"
+}
+
+
